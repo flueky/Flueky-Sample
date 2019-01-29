bd047afcf69dfd8132b0e8856d194d908bef1232
flueky <flueky@sina.com>
2019-01-29 17:48:25 +0800

    修改部分代码

--- sqlite-sample/app/src/main/java/com/flueky/demo/MainActivity.java
+++ sqlite-sample/app/src/main/java/com/flueky/demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.flueky.demo;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteCursorDriver;
 import android.database.sqlite.SQLiteDatabase;
@@ -21,39 +22,69 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            SQLiteDatabase.CursorFactory factory = new SQLiteDatabase.CursorFactory() {
+                @Override
+                public Cursor newCursor(SQLiteDatabase db, SQLiteCursorDriver masterQuery, String editTable, SQLiteQuery query) {
 
-        SQLiteDatabase.CursorFactory factory = new SQLiteDatabase.CursorFactory() {
-            @Override
-            public Cursor newCursor(SQLiteDatabase db, SQLiteCursorDriver masterQuery, String editTable, SQLiteQuery query) {
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                    return new SQLiteCursor(masterQuery, editTable, query);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        return new SQLiteCursor(masterQuery, editTable, query);
+                    } else
+                        return new SQLiteCursor(db, masterQuery, editTable, query);
                 }
-                return null;
+            };
+            DatabaseErrorHandler errorHandler = new DatabaseErrorHandler() {
+                @Override
+                public void onCorruption(SQLiteDatabase dbObj) {
+                    dbObj.close();
+                }
+            };
+//            SQLiteDatabase.OpenParams openParams = new SQLiteDatabase.OpenParams.Builder()
+//                    .setCursorFactory(factory)
+//                    .setErrorHandler(errorHandler)
+//                    .addOpenFlags(SQLiteDatabase.CREATE_IF_NECESSARY)
+//                    .build();
+
+            DBOpenHelper dbHelper = new DBOpenHelper(this, "test.db", factory, 1);
+            // 用读/写的方式打开数据库文件
+            // 第一次打开时，会创建db文件，并执行onCreate/onUpgrade 方法
+            // 需要调用 close 方法关闭
+            SQLiteDatabase database = dbHelper.getWritableDatabase();
+//        SQLiteDatabase database = SQLiteDatabase.create(factory);
+//            SQLiteDatabase database = SQLiteDatabase.createInMemory(openParams);
+//            SQLiteDatabase database = SQLiteDatabase.openDatabase(getDatabasePath("demo.db").getAbsolutePath(), factory, SQLiteDatabase.OPEN_READONLY, errorHandler);
+
+//            String CREATE_SQL = "create table user ( id INTEGER primary key autoincrement,name text,age INTEGER)";
+//            database.execSQL(CREATE_SQL);
+
+
+            ContentValues values = new ContentValues();
+            values.put("id", 1);
+            values.put("name", "flueky");
+            values.put("age", 27);
+            // 插入完整的数据，id name age
+            database.insert("user", null, values);
+            // 插入空数据，将 name 设置为 null
+            database.insert("user", "name", null);
+            // 替换 id = 1 的数据
+            values.put("id", 1);
+            values.put("name", "xiaofei");
+            values.put("age", 27);
+            database.replace("user", null, values);
+
+            Cursor cursor = database.query("user", null, null, null, null, null, null);
+
+            while (cursor.moveToNext()) {
+                int id = cursor.getInt(cursor.getColumnIndex("id"));
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                int age = cursor.getInt(cursor.getColumnIndex("age"));
+
+                Log.d(TAG, "onCreate: " + id + " " + name + " " + age);
             }
-        };
-        DBOpenHelper dbHelper = new DBOpenHelper(this, "test.db", factory, 1);
-        // 用读/写的方式打开数据库文件
-        // 第一次打开时，会创建db文件，并执行onCreate/onUpgrade 方法
-        // 需要调用 close 方法关闭
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put("name", "flueky");
-        values.put("age", 28);
-        database.insert("user", null, values);
+            cursor.close();
 
-        Cursor cursor = database.query("user", null, null, null, null, null, null);
+            database.close();
 
-        while (cursor.moveToNext()){
-            int id = cursor.getInt(cursor.getColumnIndex("id"));
-            String name = cursor.getString(cursor.getColumnIndex("name"));
-            int age= cursor.getInt(cursor.getColumnIndex("age"));
-
-            Log.d(TAG, "onCreate: "+id+" "+name+" "+age);
         }
-        cursor.close();
-
-        database.close();
-
     }
 }
--- sqlite-sample/app/src/main/java/com/flueky/demo/db/DBOpenHelper.java
+++ sqlite-sample/app/src/main/java/com/flueky/demo/db/DBOpenHelper.java
@@ -12,26 +12,31 @@ import android.os.Build;
  */
 public class DBOpenHelper extends SQLiteOpenHelper {
 
-    public DBOpenHelper( Context context,  String name,  SQLiteDatabase.CursorFactory factory, int version) {
+    // 相当于 SQLiteDatabase openDatabase(String, CursorFactory)
+    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
+    // 相当于 SQLiteDatabase openDatabase(String, CursorFactory, DatabaseErrorHandler)
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
         super(context, name, factory, version, errorHandler);
     }
 
+    // 相当于 SQLiteDatabase openDatabase(String , OpenParams);
     @TargetApi(Build.VERSION_CODES.P)
     public DBOpenHelper(Context context, String name, int version, SQLiteDatabase.OpenParams openParams) {
         super(context, name, version, openParams);
     }
 
+    // 创建数据文件时调用，此时适合创建新表
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_SQL = "create table user( id INTEGER primary key autoincrement,name text,age INTEGER)";
         db.execSQL(CREATE_SQL);
     }
 
+    // 更新数据库版本时调用，适合更新表结构或创建新表
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
